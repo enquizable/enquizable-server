082c6b19568fa31367ca376c609cd95b2ea97dab
Jordan Holmes <jordanstholmes@gmail.com>
2021-03-09 23:54:23 -0800

    Add NVM_DIR export to test-after script

--- scripts/test-after.sh
+++ scripts/test-after.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# nvm use
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm use
 npm install
