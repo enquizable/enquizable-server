ea297c466c04bb20b0a46b23da2b537d43da195f
jon-zawada <jonathan_zawada@yahoo.com>
2021-03-12 15:56:58 -0800

    adds dev dependencies - nodemon, eslint

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -9,7 +10,7 @@ app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 app.get('/', (req, res) => {
-  res.send('<h1>Dear, Jake just so you know... you rock!</h1>')
+  res.send('<h1>Dear, Jake just so you know... you rock!</h1>');
 });
 
-app.listen(3000, () => console.log('listening on 3000'))
+app.listen(3000, () => console.log('listening on 3000'));
